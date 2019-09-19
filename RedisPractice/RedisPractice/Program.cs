1f799b4f67af1ab66ae45692b1d2aa3bf2452101
Paul Wang <1043469124@qq.com>
2019-09-19 21:28:53 +0800

    redis

---
+++ RedisPractice/RedisPractice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redisCon = ConnectionMultiplexer.Connect("localhost");
+
+
+            IDatabase db = redisCon.GetDatabase();
+
+
+            db.StringSet("foo","bar");
+
+            String val = db.StringGet("foo");
+
+            Console.WriteLine(val);
+        }
+    }
+}
